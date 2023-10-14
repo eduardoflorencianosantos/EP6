42be8e80cd4c4f2b00350816acfd02f6a9c1a734
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2023-10-14 19:03:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-/* Transição da propaganda do MC Donald's */
-function mudar(objeto, imagem) {
-    trianguloretangulo.src = imagem;
-}
-
-/* Transição da propaganda de programação */
-function mudar(objeto, imagem) {
+/* Animação da propaganda de programação */
+function trocar(objeto, imagem) {
     programacao.src = imagem;
 }
+
+/* Animação da propaganda de fast food */
+function mudar(objeto, imagem) {
+    fastfood.src = imagem;
+}
+
+/* Animação da propaganda de celular */
+function animar(objeto, imagem) {
+    celular.src = imagem;
+}
+
+/* Animação da propaganda da academia */
+function deslocar(objeto, imagem) {
+    academia.src = imagem;
+}
+
+/* Está chamando o <div class="hamburger"> */
+const hamburguer = document.querySelector(".hamburguer");
+
+/* Está chamando o <ul class="nav-menu"> */
+const navMenu = document.querySelector(".nav-menu");
+
+/* Animação das três barras */
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
